766217236d73f202ed9687b1dc187a6f932e5031
Lucas Gragera <lucasgragera1@gmail.com>
2024-04-03 19:11:53 -0300

    mongo

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const connectionString = 'mongodb://127.0.0.1:27017/coder47345';
+const connectionString = 'mongodb+srv://lucasgragera51:2l0i0g1e@ecommercee.xfj5uxp.mongodb.net/?retryWrites=true&w=majority&appName=ecommercee';
 
 try {
   await mongoose.connect(connectionString);
